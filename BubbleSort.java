9db28f1c1167a23933b2b7a6128a8f73e0dc892c
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-11-27 23:25:25 +0530

    Added:Bubble sorted with user input

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,72 +5,39 @@ import java.util.Scanner;
 public class BubbleSort {
 	static Scanner scanner = new Scanner(System.in);
 
-	/*
-	 * @purpose: Ability to BubbleSort sort
-	 * 
-	 * @param: Takes integer array as input
-	 * 
-	 * @function : sorts the given array in ascending order and stores them in their
-	 * indecencies
-	 * 
-	 * @return: No return value.
-	 */
 	static public void bubbleSort(Integer[] array) {
-		for (int i = 1; i < array.length; i++) {
+//		for (int i = 1; i < array.length; i++) 
+		{
 			int size = array.length;
 
-			// loop to access each array element
-			for (int j = 0; j < size - 1; j++)
+			for (int j = 0; j < size - 1; j++) //outer loop
 
-				// loop to compare array elements
-				for (int k = 0; k < size - j - 1; k++)
+				for (int k = 0; k < size - 1 - j; k++) //Inner loop ,check the next element weather smaller then swap 
 
-					// compare two adjacent elements
-					// change > to < to sort in descending order
-					if (array[k].compareTo(array[k + 1]) > 0) {
+					if (array[k]>array[k + 1]) { //Check if the nxt element is lesser
 
-						// swapping occurs if elements
-						// are not in the intended order
-						Integer temp = array[k];
+						Integer temp = array[k]; // for swaping
 						array[k] = array[k + 1];
 						array[k + 1] = temp;
 					}
 		}
 	}
 
-	/*
-	 * @purpose: Ability to print integer array
-	 * 
-	 * @param: Takes integer array as input
-	 * 
-	 * @function : print the given integer array
-	 * 
-	 * @return: No return value.
-	 */
 	static public void printArray(Integer[] integers) {
-		for (int i = 0; i < integers.length; i++) {
+		for (int i = 0; i < integers.length; i++) { // Length of the array to print
 			System.out.println("Integer at index " + i + " is " + integers[i]);
 		}
 	}
 
-	/*
-	 * @purpose: Ability to scan integer array
-	 * 
-	 * @param: Takes integer array as input
-	 * 
-	 * @function : scans the data from user and stores in to integer array
-	 * 
-	 * @return: No return value.
-	 */
 	static public void scanArray(Integer[] integers) {
-		for (int i = 0; i < integers.length; i++) {
+		for (int i = 0; i < integers.length; i++) { //To take user input
 			System.out.printf("Enter the integer at index :%d\n", i);
 			integers[i] = scanner.nextInt();
 		}
 	}
 
 	public static void main(String[] args) {
-		Integer[] integers = new Integer[5];
+		Integer[] integers = new Integer[5];//defining the size of array
 		scanArray(integers);
 		System.out.println("***********************Before Sort*************************");
 		printArray(integers);
