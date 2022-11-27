a7b68e5c6a717ada8457245195b9f5db8df350de
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-11-27 23:27:46 +0530

    Added: Bubble sorted using user input

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,38 +6,35 @@ public class BubbleSort {
 	static Scanner scanner = new Scanner(System.in);
 
 	static public void bubbleSort(Integer[] array) {
-//		for (int i = 1; i < array.length; i++) 
 		{
 			int size = array.length;
 
-			for (int j = 0; j < size - 1; j++) //outer loop
+			for (int j = 0; j < size - 1; j++) 
+				for (int k = 0; k < size - 1 - j; k++) 
 
-				for (int k = 0; k < size - 1 - j; k++) //Inner loop ,check the next element weather smaller then swap 
+					if (array[k]>array[k + 1]) { 
 
-					if (array[k]>array[k + 1]) { //Check if the nxt element is lesser
-
-						Integer temp = array[k]; // for swaping
+						Integer temp = array[k];
 						array[k] = array[k + 1];
 						array[k + 1] = temp;
 					}
 		}
 	}
-
 	static public void printArray(Integer[] integers) {
-		for (int i = 0; i < integers.length; i++) { // Length of the array to print
+		for (int i = 0; i < integers.length; i++) { 
 			System.out.println("Integer at index " + i + " is " + integers[i]);
 		}
 	}
 
 	static public void scanArray(Integer[] integers) {
-		for (int i = 0; i < integers.length; i++) { //To take user input
+		for (int i = 0; i < integers.length; i++) { 
 			System.out.printf("Enter the integer at index :%d\n", i);
 			integers[i] = scanner.nextInt();
 		}
 	}
 
 	public static void main(String[] args) {
-		Integer[] integers = new Integer[5];//defining the size of array
+		Integer[] integers = new Integer[5];
 		scanArray(integers);
 		System.out.println("***********************Before Sort*************************");
 		printArray(integers);
